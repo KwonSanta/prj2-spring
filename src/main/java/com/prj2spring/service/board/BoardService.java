c6fb9205100829f0df895c541fcb62eee78d1291
KwonSanta <KwonSanta@gmail.com>
2024-05-21 12:35:01 +0900

    입력한 값이 비었을 경우 400 코드 리턴

--- src/main/java/com/prj2spring/service/board/BoardService.java
+++ src/main/java/com/prj2spring/service/board/BoardService.java
@@ -7,13 +7,26 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(rollbackFor = Exception.class)
 @RequiredArgsConstructor
+@Transactional(rollbackFor = Exception.class)
 public class BoardService {
 
     private final BoardMapper mapper;
 
     public void add(Board board) {
         mapper.insert(board);
+    }
+
+    public boolean validate(Board board) {
+        if (board.getTitle() == null || board.getTitle().isBlank()) {
+            return false;
+        }
+        if (board.getContent() == null || board.getContent().isBlank()) {
+            return false;
+        }
+        if (board.getWriter() == null || board.getWriter().isBlank()) {
+            return false;
+        }
+        return true;
     }
 }
