c53df8e35a348bd25ea766931d7f99ca194a0877
KwonSanta <KwonSanta@gmail.com>
2024-05-21 14:28:32 +0900

    게시물 목록 보기 [작업 완료]

--- src/main/java/com/prj2spring/service/board/BoardService.java
+++ src/main/java/com/prj2spring/service/board/BoardService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(rollbackFor = Exception.class)
@@ -28,5 +30,9 @@ public class BoardService {
             return false;
         }
         return true;
+    }
+
+    public List<Board> list() {
+        return mapper.selectAll();
     }
 }
