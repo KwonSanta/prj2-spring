25128ba61402572dd7186db3c8dac3f2a542a8b7
KwonSanta <KwonSanta@gmail.com>
2024-05-21 12:11:19 +0900

    BoardService, BoardMapper 작성
    board 테이블 생성 쿼리 작성

---
+++ src/main/java/com/prj2spring/mapper/board/BoardMapper.java
@@ -0,0 +1,16 @@
+package com.prj2spring.mapper.board;
+
+import com.prj2spring.domain.board.Board;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BoardMapper {
+
+    @Insert("""
+            INSERT INTO board
+            (title, content, writer)
+            VALUES (#{title}, #{content}, #{writer})
+            """)
+    int insert(Board board);
+}
