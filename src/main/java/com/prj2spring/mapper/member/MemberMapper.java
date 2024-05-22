8a5a45c9828466b14e763693abda1efadf18e5c6
KwonSanta <KwonSanta@gmail.com>
2024-05-22 15:21:20 +0900

    Member Read : 회원 정보 보기

--- src/main/java/com/prj2spring/mapper/member/MemberMapper.java
+++ src/main/java/com/prj2spring/mapper/member/MemberMapper.java
@@ -37,4 +37,11 @@ public interface MemberMapper {
             ORDER BY id DESC
             """)
     List<Member> selectAll();
+
+    @Select("""
+            SELECT id, email, nick_name, inserted
+            FROM member
+            WHERE id = #{id}
+            """)
+    Member selectById(Integer id);
 }
--- src/main/java/com/prj2spring/service/member/MemberService.java
+++ src/main/java/com/prj2spring/service/member/MemberService.java
@@ -59,4 +59,9 @@ public class MemberService {
     public List<Member> list() {
         return mapper.selectAll();
     }
+
+
+    public Member getByid(Integer id) {
+        return mapper.selectById(id);
+    }
 }
