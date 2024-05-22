bfe1185ebf99769a821dfdc52ce523a7dc744d30
KwonSanta <KwonSanta@gmail.com>
2024-05-22 15:10:05 +0900

    Member Read : 회원 목록 보기
    1. 가입일시 2024-05-22T14:41:02 -> 년/월/일로
    2. getMethod 는 react 에서 property 로 사용 가능

--- src/main/java/com/prj2spring/domain/member/Member.java
+++ src/main/java/com/prj2spring/domain/member/Member.java
@@ -3,6 +3,7 @@ package com.prj2spring.domain.member;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class Member {
@@ -12,4 +13,11 @@ public class Member {
     private String password;
     private String nickName;
     private LocalDateTime inserted;
+
+    // 가입일시 2024-05-22T14:41:02 -> 년/월/일로
+    // getMethod 는 react 에서 property 로 사용 가능
+    public String getSignupDateAndTime() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초");
+        return inserted.format(formatter);
+    }
 }
