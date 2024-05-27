ca4ce20227663ad53aa978618fe51a8030908aa1
KwonSanta <KwonSanta@gmail.com>
2024-05-27 12:04:08 +0900

    rightPageNumber는 lastPageNumber보다 작거나 같음

--- src/main/java/com/prj2spring/service/board/BoardService.java
+++ src/main/java/com/prj2spring/service/board/BoardService.java
@@ -37,17 +37,25 @@ public class BoardService {
 
     public Map<String, Object> list(Integer page) {
         Map pageInfo = new HashMap();
+
+        Integer firstPage = 1;
         Integer countAll = mapper.countAll();
 
         Integer offset = (page - 1) * 10;
         Integer lastPageNumber = (countAll - 1) / 10 + 1;
         Integer leftPageNumber = (page - 1) / 10 * 10 + 1;
         Integer rightPageNumber = leftPageNumber + 9;
+        rightPageNumber = Math.min(rightPageNumber, lastPageNumber);
+
+        // 처음, 이전, 다음, 맨끝
+
 
         pageInfo.put("currentPageNumber", page);
         pageInfo.put("lastPageNumber", lastPageNumber);
         pageInfo.put("leftPageNumber", leftPageNumber);
         pageInfo.put("rightPageNumber", rightPageNumber);
+        pageInfo.put("firstPageNumber", firstPage);
+        pageInfo.put("countAll", countAll);
 
         return Map.of("pageInfo", pageInfo,
                 "boardList", mapper.selectAllPaging(offset));
