d3af3c932efd826582f105a26c322edc6f53ba3a
KwonSanta <KwonSanta@gmail.com>
2024-05-27 12:12:47 +0900

    이전, 다음 버튼 페이지 계산

--- src/main/java/com/prj2spring/service/board/BoardService.java
+++ src/main/java/com/prj2spring/service/board/BoardService.java
@@ -37,8 +37,6 @@ public class BoardService {
 
     public Map<String, Object> list(Integer page) {
         Map pageInfo = new HashMap();
-
-        Integer firstPage = 1;
         Integer countAll = mapper.countAll();
 
         Integer offset = (page - 1) * 10;
@@ -46,24 +44,20 @@ public class BoardService {
         Integer leftPageNumber = (page - 1) / 10 * 10 + 1;
         Integer rightPageNumber = leftPageNumber + 9;
         rightPageNumber = Math.min(rightPageNumber, lastPageNumber);
-        // 이전
-        Integer prevPggeNumber = leftPageNumber - 1;
-        // 다음
-        Integer nextPggeNumber = rightPageNumber + 1;
+        Integer prevPageNumber = leftPageNumber - 1;
+        Integer nextPageNumber = rightPageNumber + 1;
 
-        // 처음, 이전, 다음, 맨끝
-        if (prevPggeNumber > 0) {
-            pageInfo.put("prevPggeNumber", prevPggeNumber);
+        //  이전,처음,다음,맨끝 버튼 만들기
+        if (prevPageNumber > 0) {
+            pageInfo.put("prevPageNumber", prevPageNumber);
         }
-        if (nextPggeNumber <= lastPageNumber) {
-            pageInfo.put("nextPggeNumber", nextPggeNumber);
+        if (nextPageNumber <= lastPageNumber) {
+            pageInfo.put("nextPageNumber", nextPageNumber);
         }
         pageInfo.put("currentPageNumber", page);
         pageInfo.put("lastPageNumber", lastPageNumber);
         pageInfo.put("leftPageNumber", leftPageNumber);
         pageInfo.put("rightPageNumber", rightPageNumber);
-        pageInfo.put("firstPageNumber", firstPage);
-        pageInfo.put("countAll", countAll);
 
         return Map.of("pageInfo", pageInfo,
                 "boardList", mapper.selectAllPaging(offset));
