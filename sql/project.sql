3e7773d32c519fdaa1808d5f69113773db34e6bf
KwonSanta <KwonSanta@gmail.com>
2024-05-30 11:17:40 +0900

    댓글 작성

--- sql/project.sql
+++ sql/project.sql
@@ -158,3 +158,13 @@ FROM member;
 
 SELECT *
 FROM authority;
+
+# 댓글 테이블
+CREATE TABLE comment
+(
+    id        INT PRIMARY KEY AUTO_INCREMENT,
+    board_id  INT          NOT NULL REFERENCES board (id),
+    member_id INT          NOT NULL REFERENCES member (id),
+    comment   VARCHAR(500) NOT NULL,
+    inserted  DATETIME     NOT NULL DEFAULT NOW()
+);
